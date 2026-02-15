b7df605712ad89a3f0e1a35296db43d1f739af8d
BalajiR2205 <balajirajarathnam1@gmail.com>
2026-02-15 11:21:31 +0530

    Fix array storage in Array_of_arrays.pl
    Correctly store arrays in an array of arrays.

--- Array_of_arrays.pl
+++ Array_of_arrays.pl
@@ -10,3 +10,22 @@ my @AoA = (
 $AoA[3] = [16, 17, 18, 19]; 
 
 print $AoA[3][0];
+
+# WRONG - This stores the count, not the array
+for my $i (1..10) {
+    my @array = somefunc($i);
+    $AoA[$i] = @array;  # Gets number of elements!
+}
+
+# WRONG - All references point to same array
+my @array;
+for my $i (1..10) {
+    @array = somefunc($i);
+    $AoA[$i] = \@array;  # Same reference every time!
+}
+
+# RIGHT - Creates new array each time
+for my $i (1..10) {
+    my @array = somefunc($i);
+    $AoA[$i] = [ @array ];  # Square brackets create new array
+}
